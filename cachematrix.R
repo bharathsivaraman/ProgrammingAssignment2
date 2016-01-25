8162a437f55925bb08dd7ea2c0178e89dcaed9e0
bharathsivaraman <bharathsivaram@gmail.com>
2016-01-24 22:01:11 -0500

    Update cachematrix.R
    Assignement-2. Program to check

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+#most of the code is borrowed from the example provided with changes to the variable and function names. 
+#makecacheMatrix stores four functions -> get(),set(),setinv(), and getinv()
+#list can be used to store function!!(Had trouble figuring out this part)
+#set function is used for defaulting and setting up the value of the input(Y is a global vairable, and x is recycled for every run)
+#get returns the matrix stored in the main function.It does not need any argument.
+#SetInv is used for storing the inverse of the input matrix(passing Y to set mean, Y is stored with value of X in set())
+#GetInv returns the value set in getinv
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) 
+{
+  inv <- NULL
+  set <- function(y) 
+  {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function()
+  {
+    return	x
+  }
+  
+  setinv <- function(inverse){
+    dolve(inverse)
+    inv <<- inverse
+  }
+  getinv <- function() {
+    return inv
+  }
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## This function inverses the matrix input in the previous function.Solve() is used for inversing the matrix. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data.")     
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinv(inv)
+  inv
 }
